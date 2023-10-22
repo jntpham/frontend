480e71bc1d8c922dcb7628387b3c0c5225299646
YeongsuKimm <yeongsuk505@gmail.com>
2023-10-22 10:30:43 -0700

    connect 4

--- games/connect_four/script.js
+++ games/connect_four/script.js
@@ -1,60 +1,235 @@
-const board = document.getElementById('game-board');
-const resetButton = document.getElementById('reset-button');
+//Initial references
+const container = document.querySelector(".container");
+const playerTurn = document.getElementById("playerTurn");
+const startScreen = document.querySelector(".startScreen");
+const startButton = document.getElementById("start");
+const message = document.getElementById("message");
+let initialMatrix = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+];
+let currentPlayer;
 
-const rows = 6;
-const columns = 7;
-const player1 = 'player-1';
-const player2 = 'player-2';
+//Random Number Between Range
+const generateRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min;
 
-let currentPlayer = player1;
-let isGameOver = false;
-
-function createGameBoard() {
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < columns; col++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.dataset.row = row;
-            cell.dataset.column = col;
-            board.appendChild(cell);
-        }
+//Loop through array and check for same values
+const verifyArray = (arrayElement) => {
+  let bool = false;
+  let elementCount = 0;
+  arrayElement.forEach((element, index) => {
+    if (element == currentPlayer) {
+      elementCount += 1;
+      if (elementCount == 4) {
+        bool = true;
+      }
+    } else {
+      elementCount = 0;
     }
-}
+  });
+  return bool;
+};
 
-createGameBoard();
-
-function checkForWin() {
-    // Add your win-checking logic here
-    return false;
-}
-
-function handleClick(event) {
-    if (isGameOver) return;
-
-    const cell = event.target;
-    const col = cell.dataset.column;
-
-    for (let row = rows - 1; row >= 0; row--) {
-        const targetCell = board.querySelector(`[data-row="${row}"][data-column="${col}"]`);
-        if (!targetCell.classList.contains(player1) && !targetCell.classList.contains(player2)) {
-            targetCell.classList.add(currentPlayer);
-            if (checkForWin()) {
-                alert(`Player ${currentPlayer === player1 ? 1 : 2} wins!`);
-                isGameOver = true;
-            } else {
-                currentPlayer = currentPlayer === player1 ? player2 : player1;
-            }
-            break;
-        }
+//Check for game over(Last step)
+const gameOverCheck = () => {
+  let truthCounnt = 0;
+  for (let innerArray of initialMatrix) {
+    if (innerArray.every((val) => val != 0)) {
+      truthCounnt += 1;
+    } else {
+      return false;
     }
-}
+  }
+  if (truthCounnt == 6) {
+    message.innerText = "Game Over";
+    startScreen.classList.remove("hide");
+  }
+};
 
-function resetGame() {
-    const cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => cell.className = 'cell');
-    currentPlayer = player1;
-    isGameOver = false;
-}
+//Check rows
+const checkAdjacentRowValues = (row) => {
+  return verifyArray(initialMatrix[row]);
+};
 
-board.addEventListener('click', handleClick);
-resetButton.addEventListener('click', resetGame);
+//Check columns
+const checkAdjacentColumnValues = (column) => {
+  let colWinCount = 0,
+    colWinBool = false;
+  initialMatrix.forEach((element, index) => {
+    if (element[column] == currentPlayer) {
+      colWinCount += 1;
+      if (colWinCount == 4) {
+        colWinBool = true;
+      }
+    } else {
+      colWinCount = 0;
+    }
+  });
+  //no match
+  return colWinBool;
+};
+
+//Get Right diagonal values
+const getRightDiagonal = (row, column, rowLength, columnLength) => {
+  let rowCount = row;
+  let columnCount = column;
+  let rightDiagonal = [];
+  while (rowCount > 0) {
+    if (columnCount >= columnLength - 1) {
+      break;
+    }
+    rowCount -= 1;
+    columnCount += 1;
+    rightDiagonal.unshift(initialMatrix[rowCount][columnCount]);
+  }
+  rowCount = row;
+  columnCount = column;
+  while (rowCount < rowLength) {
+    if (columnCount < 0) {
+      break;
+    }
+    rightDiagonal.push(initialMatrix[rowCount][columnCount]);
+    rowCount += 1;
+    columnCount -= 1;
+  }
+  return rightDiagonal;
+};
+
+const getLeftDiagonal = (row, column, rowLength, columnLength) => {
+  let rowCount = row;
+  let columnCount = column;
+  let leftDiagonal = [];
+  while (rowCount > 0) {
+    if (columnCount <= 0) {
+      break;
+    }
+    rowCount -= 1;
+    columnCount -= 1;
+    leftDiagonal.unshift(initialMatrix[rowCount][columnCount]);
+  }
+  rowCount = row;
+  columnCount = column;
+  while (rowCount < rowLength) {
+    if (columnCount >= columnLength) {
+      break;
+    }
+    leftDiagonal.push(initialMatrix[rowCount][columnCount]);
+    rowCount += 1;
+    columnCount += 1;
+  }
+  return leftDiagonal;
+};
+
+//Check diagonal
+const checkAdjacentDiagonalValues = (row, column) => {
+  let diagWinBool = false;
+  let tempChecks = {
+    leftTop: [],
+    rightTop: [],
+  };
+  let columnLength = initialMatrix[row].length;
+  let rowLength = initialMatrix.length;
+
+  //Store left and right diagonal array
+  tempChecks.leftTop = [
+    ...getLeftDiagonal(row, column, rowLength, columnLength),
+  ];
+
+  tempChecks.rightTop = [
+    ...getRightDiagonal(row, column, rowLength, columnLength),
+  ];
+  //check both arrays for similarities
+  diagWinBool = verifyArray(tempChecks.rightTop);
+  if (!diagWinBool) {
+    diagWinBool = verifyArray(tempChecks.leftTop);
+  }
+  return diagWinBool;
+};
+
+//Win check logic
+const winCheck = (row, column) => {
+  //if any of the functions return true we return true
+  return checkAdjacentRowValues(row)
+    ? true
+    : checkAdjacentColumnValues(column)
+    ? true
+    : checkAdjacentDiagonalValues(row, column)
+    ? true
+    : false;
+};
+
+//Sets the circle to exact points
+const setPiece = (startCount, colValue) => {
+  let rows = document.querySelectorAll(".grid-row");
+  //Initially it will place the circles in the last row else if no place availabke we will decrement the count until we find empty slot
+  if (initialMatrix[startCount][colValue] != 0) {
+    startCount -= 1;
+    setPiece(startCount, colValue);
+  } else {
+    //place circle
+    let currentRow = rows[startCount].querySelectorAll(".grid-box");
+    currentRow[colValue].classList.add("filled", `player${currentPlayer}`);
+    //Update Matrix
+    initialMatrix[startCount][colValue] = currentPlayer;
+    //Check for wins
+    if (winCheck(startCount, colValue)) {
+      message.innerHTML = `Player<span> ${currentPlayer}</span> wins`;
+      startScreen.classList.remove("hide");
+      return false;
+    }
+  }
+  //Check if all are full
+  gameOverCheck();
+};
+
+//When user clicks on a box
+const fillBox = (e) => {
+  //get column value
+  let colValue = parseInt(e.target.getAttribute("data-value"));
+  //5 because we have 6 rows (0-5)
+  setPiece(5, colValue);
+  currentPlayer = currentPlayer == 1 ? 2 : 1;
+
+  playerTurn.innerHTML = `Player <span>${currentPlayer}'s</span> turn`;
+};
+
+//Create Matrix
+const matrixCreator = () => {
+  for (let innerArray in initialMatrix) {
+    let outerDiv = document.createElement("div");
+    outerDiv.classList.add("grid-row");
+    outerDiv.setAttribute("data-value", innerArray);
+    for (let j in initialMatrix[innerArray]) {
+      //Set all matrix values to 0
+      initialMatrix[innerArray][j] = [0];
+      let innerDiv = document.createElement("div");
+      innerDiv.classList.add("grid-box");
+      innerDiv.setAttribute("data-value", j);
+      innerDiv.addEventListener("click", (e) => {
+        fillBox(e);
+      });
+      outerDiv.appendChild(innerDiv);
+    }
+    container.appendChild(outerDiv);
+  }
+};
+
+//Initialise game
+window.onload = startGame = async () => {
+  //Between 1 and 2
+  currentPlayer = generateRandomNumber(1, 3);
+  container.innerHTML = "";
+  await matrixCreator();
+  playerTurn.innerHTML = `Player <span>${currentPlayer}'s</span> turn`;
+};
+
+//start game
+startButton.addEventListener("click", () => {
+  startScreen.classList.add("hide");
+  startGame();
+});
