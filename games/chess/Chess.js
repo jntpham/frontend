98d1eaa72a70ac95be47c23b74a23830eb99efc8
YeongsuKimm <yeongsuk505@gmail.com>
2023-10-24 19:43:05 -0700

    started working on the multiplayer integration

--- games/chess/Chess.js
+++ games/chess/Chess.js
@@ -593,23 +593,33 @@ function getBoard()
     return board;
 } 
 function updateBoard() {
-    const currentBoard = getBoard();
-    console.log(currentBoard);
-    fetch('/update_board', {
+    const board = getBoard();
+    const apiurl = "https://whispbackend.duckdns.org/chessboardDB"
+      // Create a JSON object containing the board data
+    const boardData = { board };
+    console.log(boardData);
+    fetch(apiurl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ board: board }),
+        body: JSON.stringify(boardData),
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.message); // You can handle the response from Flask here
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+        .then(response => {
+            if (response.ok) {
+                return response.text(); // You can change this to JSON or another format as needed
+            } else {
+                throw new Error('Failed to send board data');
+            }
+        })
+        .then(responseData => {
+        // Handle the response from the Python script if needed
+            console.log('response:', responseData);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
 const updateInterval = setInterval(updateBoard, 100);
 
 // Moving the element
